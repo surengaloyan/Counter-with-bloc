dff9bd61ab5eff4de1a03b45b9b58f7e4ecb4d14
Suren Galoyan <sgaloyan40@gmail.com>
2022-02-26 17:00:07 +0400

    update

---
+++ lib/buttons.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'cubit/counter_cubit.dart';
+
+class CustomButtons extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('counter'),
+        ),
+        body: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Ink(
+                child: IconButton(
+                  onPressed: () {
+                    // print('hi!');
+                    BlocProvider.of<CounterCubit>(context).decrement();
+                  },
+                  icon: const Icon(Icons.remove),
+                ),
+              ),
+              BlocBuilder<CounterCubit, CounterState>(
+                builder: (context, state) {
+                  return Text(state.counterValue.toString());
+                },
+              ),
+              Ink(
+                child: IconButton(
+                  onPressed: () {
+                    BlocProvider.of<CounterCubit>(context).increment();
+                  },
+                  icon: const Icon(Icons.add),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
